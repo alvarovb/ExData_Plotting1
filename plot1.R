afe9e4801480a486ff3f3d9ab6fc6a417021cf54
Alvaro Viloria <aviloria@groupon.com>
2015-10-10 22:09:53 -0300

    adding first plot

---
+++ plot1.R
@@ -0,0 +1,15 @@
+plot1 <- function() {
+    dt <- read.csv2(file = "data/household_power_consumption.txt", na.strings = "?")
+   
+    dt <- dt[as.Date(dt$Date, "%d/%m/%Y") == "2007-02-01" | as.Date(dt$Date, "%d/%m/%Y") == "2007-02-02", ]
+  
+    dt$Global_active_power <- as.numeric(as.character(dt$Global_active_power))
+   
+    hist(dt$Global_active_power, col = "red",
+         ylab = "Frequnecy", xlab = "Global Activity Power(Kilowatts)", 
+         main  ="GLobal Activiy Power")
+    
+    dev.copy(png, file = "plot1.png") 
+    dev.off()
+    
+}
